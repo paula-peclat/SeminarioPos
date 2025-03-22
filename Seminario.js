1d88d66333fe658105f917d60db3b1de7f61f085
paula-peclat <ayla.ppeclat@gmail.com>
2025-03-21 21:43:25 -0300

    Add files via upload

---
+++ Seminario.js
@@ -0,0 +1,94 @@
+playSound("01---Super-Mario-Bros.mp3", false);
+setScreen("TL-inicio");
+onEvent("bt-segredo", "click", function( ) {
+  stopSound("");
+  setScreen("TL-acionarosegredonafamilia");
+});
+onEvent("bt-lugar", "click", function( ) {
+  stopSound("");
+  setScreen("TL-olugardoqnaopodeaprender");
+});
+onEvent("bt-paciente", "click", function( ) {
+  stopSound("");
+  setScreen("TL-afamíliadopaciente");
+});
+onEvent("bt-causalidade", "click", function( ) {
+  stopSound("");
+  setScreen("TL-causalidadeLinear");
+});
+onEvent("bt-diagnostico", "click", function( ) {
+  stopSound("");
+  setScreen("TL-diagnosticofamiliar");
+});
+onEvent("BT-maisdiag", "click", function( ) {
+  stopSound("");
+  open("https://1drv.ms/p/s!AsL82LI6zIUzgbYmQ2QQCsRtM93OSQ");
+});
+onEvent("BT-Voltardiag", "click", function( ) {
+  stopSound("");
+  setScreen("TL-inicio");
+});
+onEvent("BT-maisafamilia", "click", function( ) {
+  stopSound("");
+  setScreen("screen1");
+});
+onEvent("BT-iniciofamiliapac", "click", function( ) {
+  stopSound("");
+  setScreen("TL-inicio");
+});
+onEvent("bt-voltarfamiliapaciente2", "click", function( ) {
+  stopSound("");
+  setScreen("TL-afamíliadopaciente");
+});
+onEvent("BT-Voltarcausalid", "click", function( ) {
+  stopSound("");
+  setScreen("TL-inicio");
+});
+onEvent("BT-Voltarfamiliafamilia", "click", function( ) {
+  stopSound("");
+  setScreen("TL-inicio");
+});
+onEvent("bt-famila", "click", function( ) {
+  stopSound("");
+  setScreen("TL-afamiliaeafamilia");
+});
+onEvent("bt-lugar", "click", function( ) {
+  stopSound("");
+  setScreen("TL-olugardoqnaopodeaprender");
+});
+onEvent("Bt-voltarlugardoque", "click", function( ) {
+  stopSound("");
+  setScreen("TL-inicio");
+});
+onEvent("BT-Playnemo", "click", function( ) {
+  stopSound("");
+  open("https://drive.google.com/file/d/1mFsoKBEhE17wJpCDfRHQLJ0niogQWcYI/view?usp=sharing");
+});
+onEvent("Bt-playligi", "click", function( ) {
+  stopSound("");
+  open("https://drive.google.com/file/d/147Odzs1laLentoR9KqZzZBXx_9LXhCxr/view?usp=share_link");
+});
+onEvent("bt-playmario", "click", function( ) {
+  stopSound("");
+  open("https://drive.google.com/file/d/1irzgUM3JzG6o09WV2-V7rniSQMOtr6cQ/view?usp=share_link");
+});
+onEvent("BT-Voltar-acionar", "click", function( ) {
+  stopSound("");
+  setScreen("TL-inicio");
+});
+onEvent("BT-Voltarfamiliafamilia", "click", function( ) {
+  stopSound("");
+  setScreen("TL-inicio");
+});
+onEvent("BT-voltarimagem", "click", function( ) {
+  stopSound("");
+  setScreen("TL-olugardoqnaopodeaprender");
+});
+onEvent("button2", "click", function( ) {
+  stopSound("");
+  setScreen("TL-imagemlugar");
+});
+onEvent("button1", "click", function( ) {
+  stopSound("");
+  open("https://studio.code.org/projects/gamelab/hoDIZ53lJNyXCRtVYeagQQMP5T8jcd4ETa4oTomfOIU/edit");
+});
